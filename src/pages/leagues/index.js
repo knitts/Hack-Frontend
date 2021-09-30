bcad3cda069b9e72118d182ecb896aaf73282afe
thrishik7 <thrishik.s@gmail.com>
2021-09-30 22:28:39 +0530

    integrated list of leagues

--- src/pages/leagues/index.js
+++ src/pages/leagues/index.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react'
 import Sidebar from '../../components/sidebar/sidebar'
 import Navbar from '../../components/navbar/navbar'
-import "../../web3"
+import {Link} from 'react-router-dom'
+import web3 from "../../web3"
 
-export default function index() {
+const Knitts = require("../../build/contracts/Knitts.json")
+var gasfee = 5e6;
+
+class index extends React.Component {
+  
+  constructor(){
+    super();
+    this.state={
+      leagues: []
+    }
+    
+  }
+  
+  async componentDidMount(){
+    let accounts = await web3.eth.getAccounts();
+    let organization = accounts[0];
+    let moderator = accounts[0];
+    let randomAccount = accounts[8];
+    
+    let knitts = await new web3.eth.Contract(Knitts.abi, {from:organization});
+    knitts = await knitts.deploy({data:Knitts.bytecode}).send({from:organization, gas: gasfee});
+      
+    let res = await knitts.methods.getDetails().call();
+    console.log(res[0]);
+    this.setState({leagues:res[0]});
+  }
+
+  render(){
+  var leagues = this.state.leagues;
   return (
     <>
     <div className="overflow-x-hidden text-white" style={{"backgroundImage":"url('./bg_1.jpg')","backgroundPosition":"fixed","backgroundSize":"cover","backgroundRepeat":"no-repeat"}} >
@@ -14,61 +43,26 @@ export default function index() {
               <h2 class="text-4xl font-bold tracking-tight text-center">Leagues</h2>
               <p class="mt-2 text-lg text-center text-white">Check out our list of awesome leagues below.</p>
               <div class="grid grid-cols-4 gap-8 mt-10 sm:grid-cols-8 lg:grid-cols-12 sm:px-8 xl:px-0">
-
+                {leagues.map(function(league, index){
+                    return ( 
                   <button class="relative flex flex-col shadow bg-opacity-90 shadow items-center justify-between col-span-4 px-8 py-12 overflow-hidden bg-gray-800 sm:rounded-xl">
-                      <div class="p-3 mb-6 text-white bg-blue-500 rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                          <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
-                        </svg>
-                      </div>
-                      <h4 class="text-xl font-bold text-white">League 1</h4>
-                  </button>
-
-                  <button class="relative flex flex-col shadow bg-opacity-90 shadow items-center justify-between col-span-4 px-8 py-12 overflow-hidden bg-gray-800 sm:rounded-xl">
-                      <div class="p-3 mb-6 text-white bg-blue-500 rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                          <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
-                        </svg>
-                      </div>
-                      <h4 class="text-xl font-bold text-white">League 2</h4>
-                  </button>
-
-                  <button class="relative flex flex-col shadow bg-opacity-90 shadow items-center justify-between col-span-4 px-8 py-12 overflow-hidden bg-gray-800 sm:rounded-xl">
-                      <div class="p-3 mb-6 text-white bg-blue-500 rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                          <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
-                        </svg>
-                      </div>
-                      <h4 class="text-xl font-bold text-white">League 3</h4>
-                  </button>
-
-                  <button class="relative flex flex-col shadow bg-opacity-90 shadow items-center justify-between col-span-4 px-8 py-12 overflow-hidden bg-gray-800 sm:rounded-xl">
-                      <div class="p-3 mb-6 text-white bg-blue-500 rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                          <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
-                        </svg>
-                      </div>
-                      <h4 class="text-xl font-bold text-white">League 4</h4>
-                  </button>
-
-                  <button class="relative flex flex-col shadow bg-opacity-90 shadow items-center justify-between col-span-4 px-8 py-12 overflow-hidden bg-gray-800 sm:rounded-xl">
-                      <div class="p-3 mb-6 text-white bg-blue-500 rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                          <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
-                        </svg>
-                      </div>
-                      <h4 class="text-xl font-bold text-white">League 5</h4>
-                  </button>
-
-                  <button class="relative flex flex-col shadow bg-opacity-90 shadow items-center justify-between col-span-4 px-8 py-12 overflow-hidden bg-indigo-900 sm:rounded-xl">
+                    <div class="p-3 mb-6 text-white bg-blue-500 rounded-full">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
+                      </svg>
+                    </div>
+                    <h4 class="text-xl font-bold text-white">League {index+1}</h4>
+                </button>);
+                  })}
+                
+                  <Link to="/CreateLeague" class="relative flex flex-col shadow bg-opacity-90 shadow items-center justify-between col-span-4 px-8 py-12 overflow-hidden bg-indigo-900 sm:rounded-xl">
                       <div class="p-3 mb-6 text-white bg-blue-500 rounded-full">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                           <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
                         </svg>
                       </div>
                       <h4 class="text-xl font-bold text-white">Create a new League</h4>
-                  </button>
-
+                </Link>
               </div>
           </div>
       </section>
@@ -77,5 +71,6 @@ export default function index() {
       
     </>
     )
-}
+}}
 
+export default index;
