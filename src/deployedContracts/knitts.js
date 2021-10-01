5065ba674251c7d5a357b13998d780f2f89bd236
Arvinth_s <arvinth.saravana@gmail.com>
2021-10-01 20:08:17 +0530

    added gitignore to knitts

--- src/deployedContracts/knitts.js
+++ src/deployedContracts/knitts.js
@@ -1,9 +1,9 @@
 import web3 from '../web3';
 import Knitts from '../build/contracts/Knitts.json';
+import address from './address.json';
 
 const instance = new web3.eth.Contract(
 	Knitts.abi,
-	//address of crowdly here
-	'0x9D25f243aD38E54A8e11cF4C4c65B24831235684'
+	address.knitts,
 );
 export default instance;
