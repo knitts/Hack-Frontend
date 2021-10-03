4b539a87095351903c886ce3490bfa7d24cc8597
Arvinth_s <arvinth.saravana@gmail.com>
2021-10-03 16:24:15 +0530

    projects page updated

--- src/deployedContracts/Leagues.js
+++ src/deployedContracts/Leagues.js
@@ -4,7 +4,6 @@ import League from '../build/contracts/League.json';
 function instance(address) {
     return new web3.eth.Contract(
     League.abi,
-	//address of crowdly here
 	address
     );
 }
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import {Link} from 'react-router-dom'
 import { useLocation } from 'react-router-dom';
 import Sidebar from '../../components/sidebar/sidebar'
 import Navbar from '../../components/navbar/navbar'
-import "../../web3"
+import Leagues from '../../deployedContracts/Leagues'
+import web3 from "../../web3"
+import League from '../../build/contracts/League.json'
 
 export default function Index() {
-  // const location = useLocation()
-  // const { leagueAdd } = location.state
-  // console.log(leagueAdd);
+  const location = useLocation()
+  const { leagueAdd } = location.state
+  console.log(leagueAdd);
+  let [projects, setProjects] = useState([]);
+
+  useEffect( async () => {
+    console.log('leagueAdd', leagueAdd['league']);
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+    let league = Leagues(leagueAdd['league']);
+    console.log('league', await league.options.address)
+    let _projects = []
+    let numProjects = await league.methods.numProjects().call();
+    for(let i=0; i<numProjects;i++){
+      _projects.push(await league.methods.submissionDetails(i).call());
+    }
+    console.log('projects', _projects);
+
+    setProjects(_projects);
+  }, [])
+
+
   return (
     <>
     <div className="overflow-x-hidden text-white" style={{"backgroundImage":"url('./login_bg1.jpg')","backgroundPosition":"fixed","backgroundSize":"cover","backgroundRepeat":"no-repeat"}} >
@@ -19,15 +40,17 @@ export default function Index() {
           <div class="container max-w-6xl mx-auto">
               <h2 class="text-4xl font-bold tracking-tight text-center">Projects submitted to this League are shown below</h2>
               <div class="grid grid-cols-4 gap-8 mt-10 sm:grid-cols-8 lg:grid-cols-12 sm:px-8 xl:px-0">
-
-                  <button class="relative flex flex-col shadow bg-opacity-90 shadow items-center justify-between col-span-4 px-8 py-12 overflow-hidden bg-gray-800 sm:rounded-xl">
-                      <div class="p-3 mb-6 text-white bg-blue-500 rounded-full">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                          <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
-                        </svg>
-                      </div>
-                      <h4 class="text-xl font-bold text-white">Crowdly</h4>
-                  </button>
+                {projects.map(function(project, index){
+                    <button class="relative flex flex-col shadow bg-opacity-90 shadow items-center justify-between col-span-4 px-8 py-12 overflow-hidden bg-gray-800 sm:rounded-xl">
+                    <div class="p-3 mb-6 text-white bg-blue-500 rounded-full">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
+                      </svg>
+                    </div>
+                    <h4 class="text-xl font-bold text-white">{index} {project}</h4>
+                </button>
+                })}
+                  
 
                   
                   <button class="relative flex flex-col shadow bg-opacity-90 shadow items-center justify-between col-span-4 px-8 py-12 overflow-hidden bg-gray-800 sm:rounded-xl">
