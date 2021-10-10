6a92157a052bf14e6e4745234f2ab29e290e55a3
Abhishec177 <abhishek122k@gmail.com>
2021-10-11 00:33:48 +0530

    integrated packages

--- src/web3.js
+++ src/web3.js
@@ -16,5 +16,10 @@ else if(typeof window !=='undefined' && typeof web3 !== 'undefined')
     web3 = new Web3(web3Provider)
 }
 
+// import Portis from '@portis/web3';
+// import Web3 from 'web3';
+
+// const portis = new Portis('bc6bf960-1231-411d-83ef-2fa48e0cf1cb', 'maticMumbai');
+// const web3 = new Web3(portis.provider);
 
 export default web3;
